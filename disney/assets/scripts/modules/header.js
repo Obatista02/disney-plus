41797d67a684c1b3df07ee077102f720324aa255
Obatista02 <guilherme.santos201412@gmail.com>
2023-03-24 19:33:08 -0300

    Acrescentando o JavaScript

---
+++ disney/assets/scripts/modules/header.js
@@ -0,0 +1,39 @@
+const header = document.querySelector('[data-header]')
+const openNAvSubMenu = document.querySelector('[data-open-navSubMenu]')
+const navSubMenu = document.querySelector('[data-navSubMenu]')
+const userMenu = document.querySelector('[data-usermenu]')
+const openUserMenu = document.querySelector('[data-open-usermenu]')
+
+
+
+function onWindowScroll(){
+    if(window.scrollY > 20){
+      header.style.backgroundColor = '#0C0D14'
+    }else{
+      header.style.backgroundColor = 'transparent'
+    }
+  }
+
+function onTouchOpenNavSubMenu(event){
+  event.preventDefault()
+  navSubMenu.classList.toggle('active')
+}
+
+function onTouchOpenUserMenu(event){
+  event.preventDefault()
+  userMenu.classList.toggle('active')
+}
+
+function setListeners(){
+  window.addEventListener('scroll',onWindowScroll)
+  openNAvSubMenu.addEventListener('touchstart', onTouchOpenNavSubMenu)
+  openUserMenu.addEventListener('touchstart', onTouchOpenUserMenu)
+}
+
+function init(){
+  setListeners()
+}
+
+export default{
+  init
+}
